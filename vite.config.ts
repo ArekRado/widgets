9555246eddfc0f8b6a8444742a4182391f17f318
Arek Rado <arekrrr@gmail.com>
2023-02-01 22:45:11 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: './',
   build: {
     rollupOptions: {
       output: {
@@ -9,5 +10,5 @@ export default defineConfig({
         entryFileNames: '[name]-[hash].js',
       },
     },
-  }
+  },
 })
